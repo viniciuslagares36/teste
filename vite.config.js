b2e72168554548162b03986f75b2ae1316d11c76
Vinicius <viniciuslagares36@gmail.com>
2026-04-29 11:46:30 -0300

    test

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
+    port: 3000
   }
 })
